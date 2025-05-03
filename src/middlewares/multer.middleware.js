ac26a08a4c53f16290376d9991ade7c2559d9fcb
Saurav Karki <sauravkarki10.12@gmail.com>
2025-05-03 23:48:36 +0545

    modified: removed local storage

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -4,9 +4,9 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./public/temp");
-    },
+    // destination: function (req, file, cb) {
+    //     cb(null, "./public/temp");
+    // },
     filename: function (req, file, cb) {
  
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
